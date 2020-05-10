7490b0df59967e7de622128203b194c2484c21ce
jonasttc <jonas@technocreatives.com>
2020-05-11 01:44:40 +0200

    Import Unity project (with correct .gitignore immediately this time)

---
+++ Unity_Project/Yesipe/Assets/Scripts/BlobAreaController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BlobAreaController : MonoBehaviour
+{
+    public Vector3 targetPos;
+    Vector3 refPos;
+    public bool disablePaning, panWithForce;
+
+    BlobController[] blobs;
+
+    void Start()
+    {
+        targetPos = transform.position;
+        blobs = GetComponentsInChildren<BlobController>();
+    }
+
+    void Update()
+    {
+        if(!panWithForce)
+            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref refPos, 0.3f);
+    }
+
+    public void PanBlobs(Vector2 move)
+    {
+        if (disablePaning)
+        {
+            return;
+        }
+
+        if (panWithForce)
+        {
+            move = move / Screen.width * 30000f;
+
+            foreach (var blob in blobs)
+            {
+                blob.rigidBody2D.AddForce(move);
+            }
+        }
+
+        targetPos += (Vector3)move / Screen.width * 30f;
+    }
+
+    public void PanBlobs(BaseEventData eventData)
+    {
+        PointerEventData pointerEventData = eventData as PointerEventData;
+        PanBlobs(pointerEventData.delta);
+    }
+}
+
+[Serializable]
+public struct YesipeBubbleDatas
+{
+
+}
---
+++ Unity_Project/Yesipe/Assets/Scripts/BlobController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+[ExecuteAlways]
+public class BlobController : MonoBehaviour, IDragHandler
+{
+    public bool panWithForce;
+
+    public Color color;
+    public float size = 1f, blobNoise = 2f;
+    [Range(0f, 0.8f)]
+    public float blobAmplitude = 0.3f;
+    int maxColliders = 4;
+
+    List<BlobController> colliders = new List<BlobController>();
+    [HideInInspector]
+    public Rigidbody2D rigidBody2D;
+    SpriteRenderer sprite;
+    RawImage rawImage;
+    
+    Material tempMaterial;
+    TMP_Text text;
+
+    BlobAreaController blobArea;
+
+    private void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        rawImage = GetComponent<RawImage>();
+        text = GetComponentInChildren<TMP_Text>();
+        blobArea = GetComponentInParent<BlobAreaController>();
+
+        CreateAndAssignMaterial();
+        UpdateBlobProperties();
+    }
+
+    void CreateAndAssignMaterial()
+    {
+        tempMaterial = new Material(Shader.Find("Shader Graphs/Blob Shader 2D"));
+        tempMaterial.SetVector("_Color", color);
+        if (sprite)
+            sprite.material = tempMaterial;
+        if (rawImage)
+            rawImage.material = tempMaterial;
+    }
+
+    void UpdateBlobProperties()
+    {
+        tempMaterial.SetFloat("_OwnSize", size);
+        tempMaterial.SetFloat("_BlobAmplitude", blobAmplitude);
+        tempMaterial.SetFloat("_BlobNoise", blobNoise);
+    }
+
+    void Update()
+    {
+        if (!tempMaterial)
+            CreateAndAssignMaterial();
+
+        tempMaterial.SetVector("_SelfPos", transform.position);
+
+        colliders = new List<BlobController>();
+        foreach (var item in Physics2D.OverlapCircleAll(transform.position, 8f))
+        {
+            if (item.gameObject != gameObject)
+            {
+                colliders.Add(item.GetComponent<BlobController>());
+            }
+        }
+
+        for (int i = 0; i < colliders.Count; i++)
+        {
+            Vector4 input = colliders[i].transform.position;
+            input.w = colliders[i].size * Mathf.Pow(colliders[i].transform.localScale.x, 2);
+            tempMaterial.SetVector($"Collider{i + 1}", input);
+        }
+
+        for (int i = colliders.Count; i <= maxColliders; i++)
+        {
+            tempMaterial.SetVector($"Collider{i + 1}", new Vector4());
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (panWithForce)
+        {
+            rigidBody2D.AddForce(eventData.delta / Screen.width * 15000f);
+            blobArea.PanBlobs(eventData.delta / 4f);
+        }
+        else
+        {
+            rigidBody2D.MovePosition(eventData.pointerCurrentRaycast.worldPosition);
+            blobArea.PanBlobs(eventData.delta / 2f);
+        }
+    }
+}
